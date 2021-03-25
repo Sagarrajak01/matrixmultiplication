84d531b823b3fe4473cf771f7038b523d3e9871c
Sagarrajak01 <65167758+Sagarrajak01@users.noreply.github.com>
2021-03-25 08:31:09 +0530

    matrixmultiplication

--- MATRIX-M.C
+++ MATRIX-M.C
@@ -1,65 +1,66 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<conio.h>
 int main()
 {
-int a[10][10],b[10][10],c[10][10];
-int i,j,k,ra,ca,rb,cb;
-printf("Input rows for matrix A =");
-scanf("%d",&ra);
-printf("Input columns for matrix A =");
-scanf("%d",&ca);
-printf("Input rows for matrix B =");
-scanf("%d",&rb);
-printf("Input columns for matrix B =");
-scanf("%d",&cb);
-/* printf("\n\t Matrix A \n");
-printf("------------------");
-for(i=0;i<ra;i++)
-{
-for(j= 0;j<ca;j++)
-{
-printf */
-if(ca==rb)
-{
-printf("Input %d elements for matrix A",ra*ca);
-for (i=0;i<ra;i++)
-{
-for(j=0;j<ca;j++)
-{
-scanf("%d",&a[i][j]);
-}
-}
-printf("Input %d elements for matrix B",rb*cb);
-for(i=0;i<rb;i++)
-{
-for(j=0;j<cb;j++)
-{
-scanf("%d",&b[i][j]);
-}
-}
-for(i=0;i<ra;i++)
-{
-for(j=0;j<ca;j++)
-{
-c[i][j]=0;
-for(k=0;k<ra*cb;k++)
-{
-c[i][j]=c[i][j]+a[i][k]*b[k][j];
-}
-}
-}
-printf("After multiplication of matrix are \n");
-for(i=0;i<ra;i++)
-{
-for(j=0;j<cb;j++)
-{
-printf("%d ",c[i][j]);
-}
-printf("\n");
-}
-}
-else
-{
-printf("Invalid Rows and columns");
-}
-return 0;
+  
+  int a[10][10],b[10][10],c[10][10];
+  int i,j,k,m,n,p,q;
+  clrscr();
+  l:
+  printf("Input row size of matrix A ");
+  scanf("%d",&m);
+  printf("\n Input column size of matrix A ");
+  scanf("%d",&n);
+  printf("\nInput row size of matrix B ");
+  scanf("%d",&p);
+  printf("\nInput column size of matrix B ");
+  scanf("%d",&q);
+  if(n!=p){
+  goto l;
+  }
+  printf("\nInput %d elements for Matrix A ",m*n);
+  for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+      scanf("%d",&a[i][j]);
+    }
+  }
+  printf("\nInput %d elements for Matrix B ",p*q);
+  for(i=0;i<p;i++){
+    for(j=0;j<q;j++){
+      scanf("%d",&b[i][j]);
+    }
+  }
+  for(i=0;i<m;i++){
+    for(j=0;j<q;j++){
+      c[i][j]=0;
+      for(k=0;k<n;k++){
+        c[i][j]=c[i][j]+a[i][k]*b[k][j];
+      }
+    }
+  }
+  printf("\nElements of  Matrix A is\n");
+  for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+      printf("%d ",a[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\nElements of  Matrix B is\n");
+  for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+      printf("%d ",b[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\nAfter multiplication of Matrix A and B \n");
+  for(i=0;i<m;i++){
+    for(j=0;j<q;j++){
+      printf("%d ",c[i][j]);
+     }
+      printf("\n");
+  }
+   
+    
+  return 0;
+  getch();
 }
